1a3baea6fec6515cd3b4662b1370f72be9c04e13
Rio <riostockton@gmail.com>
2021-04-11 15:57:21 +0100

    reconfigured react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Route path="/" exact component={App} />
   </Router>,
   document.getElementById('root')
 );

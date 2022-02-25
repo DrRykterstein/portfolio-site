c5224207999a2def57cf679df20772406f0cfc50
Rio <riostockton@gmail.com>
2022-02-25 09:26:31 +0000

    Updated portfolio items data

--- src/data/portfolioItems.tsx
+++ src/data/portfolioItems.tsx
@@ -7,14 +7,14 @@ const portfolioItems: PortfolioItems = {
 		src: "bug-tracker.png",
 		github: "bug-tracker",
 		link: "https://dotnet-bug-tracker.herokuapp.com/",
-		desc: "A bug/issue tracking application built in C# Asp.Net MVC that tracks and maintains issues in the form of tickets for a collection of projects. Includes authentication and role-based authorization with Identity.",
+		desc: "A bug/issue tracking application built with C# ASP.NET MVC that stores and maintains issues in the form of tickets for a collection of projects. Includes authentication and role-based authorization with Identity.",
 	},
 	techBlog: {
 		title: "Technology Blog",
 		src: "tech-blog.png",
 		github: "tech-blog-server",
 		link: "https://tech-blog-three.vercel.app/blog",
-		desc: "A programming tutorial blog built in C# Asp.Net Web API and React that implements JWT authentication, claims-based authorization and entity framework data access.",
+		desc: "A programming tutorial blog built with C# ASP.NET Web API and React that implements JWT authentication, claims-based authorization, and entity framework data access.",
 	},
 	// wired: {
 	// 	title: "Wired",
@@ -35,21 +35,21 @@ const portfolioItems: PortfolioItems = {
 		src: "book-store.png",
 		github: "infinite-book-store",
 		link: "https://book-store-nine.vercel.app",
-		desc: "A Book store built in NextJS, Typescript, and Shopify with headless integration as well as full firebase authentication. Products are fetched using GraphQL through Shopify's storefront API.",
+		desc: "A Book store built with NextJS, Typescript, and Shopify with headless integration as well as full firebase authentication. Products are fetched using GraphQL through Shopify's storefront API.",
+	},
+	taskTracker: {
+		title: "Task Tracker",
+		src: "task-tracker.png",
+		github: "task-tracker",
+		link: "https://optimistic-poitras-267ce4.netlify.app",
+		desc: "A task tracking application built with React and structured using class components. Includes CRUD functionality as well as MySQL database integration and displays a background image fetched using the Unsplash REST API.",
 	},
 	countryLibrary: {
 		title: "Country Library",
 		src: "country-library.png",
 		github: "infinite-country-library",
 		link: "https://modest-volhard-e48a9a.netlify.app",
-		desc: "A website built in HTML, CSS and Javascript that displays information regarding a specific country based on data fetched from an external API.",
-	},
-	todoApp: {
-		title: "Todo App",
-		src: "todo-app.png",
-		github: "react-todo-app",
-		link: "https://optimistic-poitras-267ce4.netlify.app",
-		desc: "A Todo List application built in React and structured using class components. Includes full CRUD functionality as well as MySQL database integration and displays a background image fetched using the Unsplash REST API.",
+		desc: "A website built with HTML, CSS and Javascript that displays information regarding a specific country based on data fetched from an external API.",
 	},
 };
 
